10f161f847d1d010d466cc365845e9a291b135f5
kunal-new <nishantdeep121@gmail.com>
2025-12-16 16:22:42 +0530

    Test files

---
+++ Tests/test3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int x = 10;
+    x += 5;          // breakpoint here
+    std::cout << x << std::endl;
+    return 0;
+}
---
+++ Tests/test4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        std::cout << i << std::endl;  // breakpoint here
+    }
+    return 0;
+}
